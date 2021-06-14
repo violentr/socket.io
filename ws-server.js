9a1bdabad55cb2183507331aa561d5844dcef9c0
deniss <den.b@itservicesgroup.co.uk>
2021-06-14 17:27:40 +0100

    Add Server, logic how to process message

---
+++ ws-server.js
@@ -0,0 +1,20 @@
+// Simple Websocket server
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  console.log(res)
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log(`Emitted: ${msg}`)
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(5000, function(){
+  console.log('listening on *:5000');
+});
