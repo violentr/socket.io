8060ef38821015d71c76bc5cbae375a3c8cc532e
deniss <den.b@itservicesgroup.co.uk>
2021-06-15 16:39:45 +0100

    Add Server, communication with custom namespace

--- example1/ws-server.js
+++ example1/ws-server.js
@@ -1,19 +1,29 @@
 // Simple Websocket server
 var app = require('express')();
 var http = require('http').Server(app);
+// var options = {path: "/my-custom-path/"}
 var io = require('socket.io')(http);
+const mySpace = io.of("/my-space");
 
 app.get('/', function(req, res){
-  console.log(res)
+  console.log(req)
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+// main namespace defined with "/"
+io.of('/').on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log(`Emitted: ${msg}`)
     io.emit('chat message', msg);
   });
 });
+
+mySpace.on('connection' , function(socket){
+  socket.on('my space', function(msg){
+    console.log(`My space: ${msg}`);
+    io.emit('my space', msg);
+  })
+})
 
 http.listen(5000, function(){
   console.log('listening on *:5000');
