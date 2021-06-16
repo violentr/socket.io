d42524570e8799c4bcfff0a877effc0f1bb21858
deniss <den.b@itservicesgroup.co.uk>
2021-06-16 14:04:58 +0100

    Add Client read data from custom namespace

--- example1/ws-client.js
+++ example1/ws-client.js
@@ -1,16 +1,22 @@
 const io = require("socket.io-client");
 // let options = {path: "/my-custom-path/"}
-const url = "ws://localhost:5000"
+let url = "ws://localhost:5000"
 const socket = io(url);
 
 // Read data from main namespace
-socket.on("chat message", data => {
-  console.log(`Message: ${data}`);
+socket.on("chat message", function(data) {
+  console.log(`Message from main namespace: ${data}`);
 });
 
 // Connect and read data from custom namespace
-socket.connect('/my-space').on("my space", data => {
-  console.log(`Message from custom namespace: ${data}`);
+// For some reason const mySpace = socket.connect('/my-space');
+// Does not work, should do work around
+
+let spaceUrl = "ws://localhost:5000/my-space"
+const mySpace = io(spaceUrl) ;
+
+mySpace.on("my space", function(data) {
+  console.log(`Message from my-space namespace: ${data}`);
 });
 
 console.log(`[+] App started successfully`)
