659930cd421ffe71f310bb655689b426831a1e03
deniss <den.b@itservicesgroup.co.uk>
2021-06-16 13:29:27 +0100

    Add Make sure response comming from namespaced socket

--- example1/ws-server.js
+++ example1/ws-server.js
@@ -6,22 +6,23 @@ var io = require('socket.io')(http);
 const mySpace = io.of("/my-space");
 
 app.get('/', function(req, res){
-  console.log(req)
+  // console.log(req)
   res.sendFile(__dirname + '/index.html');
 });
 
 // main namespace defined with "/"
 io.of('/').on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log(`Emitted: ${msg}`)
+    console.log(`Response from main namespace: ${msg}`)
     io.emit('chat message', msg);
   });
 });
 
+// read from custom namespace "my-space"
 mySpace.on('connection' , function(socket){
   socket.on('my space', function(msg){
-    console.log(`My space: ${msg}`);
-    io.emit('my space', msg);
+    console.log(`Response from my-space namespace: ${msg}`);
+    mySpace.emit('my space', msg);
   })
 })
 
